64eba36fc25283f3ffaf9dfe1803c81e5fe71083
Preshaa <jainpreeti2409@gmail.com>
2017-01-31 18:28:28 +0530

    mend

--- KeyPop.js
+++ KeyPop.js
@@ -1,35 +1,49 @@
 $(document).ready(function(){
-	                   var text="";
-						$("#start").click(function(){
-						
-							var counter=10,i=0;
-							
-			        var interval = setInterval(function(){
-			        	counter--;
-								var t = Math.round((Math.random() * 500));
-				        var l = Math.round((Math.random() * 500));
-				        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	              var c = possible.charAt(Math.floor(Math.random() * possible.length));
-				        console.log(t+" "+l+" "+c); 
-					      $("#pop").css({"left":l,"top":t});
-					      $("#key").text(c);
-					      text+=c;
-					      console.log(text);
-					      $("#key").css({"text-align":"center","margin":"auto"});
-					      $("span#text").text(counter);
-	 			        if (counter==0) {
-					      	$("span#text").text("Timeout");
-					        clearInterval(interval);
-	              }
-	            },1000);
-          });
-						$("#submit").click(function(){
-                             console.log(text);
-                             console.log($("textarea").val());
-					        if (text===$("textarea").val()) {
-	              $("span#result").text("success");
-	       	    }
-	       	    else
-	       		    $("span#result").text("failed!try again");
-						});
-	        });  
+  var text="";
+  $("button#submit").prop('disabled',true);
+	$("#start").click(function(){		
+	   $("button.initiate").prop('disabled',true);				
+		var counter = 10, i = 0;
+		var interval = setInterval(function(){     
+		  counter --;
+			var t = Math.round((Math.random() * 100) - 1);
+		  var l = Math.round((Math.random() * 100) - 1);                            //random position from top and left
+		  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+		  var c = possible.charAt(Math.floor(Math.random() * possible.length)); //random selection of alphabet from possible characters
+		  console.log(t + " " + l + " " + c); 
+			$("#pop").css({"left":l+"%","top":t+"%"});
+			$("#key").text(c);
+			text += c;
+			console.log(text);
+			$("#key").css({"text-align":"center","margin":"auto"});
+			$("span#text").text(counter);
+		 	if (counter == 0) {
+			  $("span#text").text("Timeout");
+				clearInterval(interval);
+				$("button#submit").prop('disabled',false);
+				if ( $("textarea").val()== ""){
+					text="";
+	        $(".Popup").append("failed!!");
+	        $(".Popup").popup();
+	      }
+			}
+	  },1000);
+  });
+	$("#submit").click(function(){
+    console.log(text);
+    console.log($("textarea").val());
+	  if (text === $("textarea").val()){
+	  	text="";
+	    $(".Popup").append("success");
+	    $(".Popup").popup();
+	    $("textarea").val("");
+	  }
+	  else{
+	  	text="";
+	    $(".Popup").append("failed!try again");
+	    $(".Popup").popup();
+	    $("textarea").val("");
+	  }
+	  
+	});
+});  
